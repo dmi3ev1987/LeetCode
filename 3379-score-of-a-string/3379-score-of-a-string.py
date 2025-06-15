56e5293a658f3d7eaf7ec8885581c0a51cb3e74b
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-06-15 15:30:32 +0000

    Score of a String.

--- 3379-score-of-a-string/3379-score-of-a-string.py
+++ 3379-score-of-a-string/3379-score-of-a-string.py
@@ -3,5 +3,5 @@ class Solution:
         result = 0
         ascii_values = [ord(letter) for letter in input_string]
         for index in range(len(input_string) - 1):
-            result += abs(ascii_values[index] - ascii_values[index+1])
+            result += abs(ascii_values[index] - ascii_values[index + 1])
         return result
