12c9445188df5bcfd25ff1bbd170502a3ffab2d3
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-06-27 15:25:05 +0000

    nsert Greatest Common Divisors in Linked List.

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.py
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.py
@@ -4,16 +4,17 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def insertGreatestCommonDivisors(
+        self, head: Optional[ListNode]
+    ) -> Optional[ListNode]:
         current = head
         while current.next:
             value = current.val
             next_value = current.next.val
             max_common_divisor = 0
             for number in range(1, min(value, next_value) + 1):
-                if value % number == 0 and next_value % number ==0:
+                if value % number == 0 and next_value % number == 0:
                     max_common_divisor = number
             current.next = ListNode(max_common_divisor, next=current.next)
             current = current.next.next
         return head
-        
