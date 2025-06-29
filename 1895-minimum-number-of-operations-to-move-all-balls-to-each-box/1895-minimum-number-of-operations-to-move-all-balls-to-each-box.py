5a3c9ab3ee6039eaede35cd59fe0fead5307683e
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-29 13:55:03 +0300

    Time: 2444 ms (33.44%), Space: 18.4 MB (6.08%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,15 +1,15 @@
 class Solution:
     def minOperations(self, boxes: str) -> list[int]:
-        index_list = [
+        enumerated_list = [
             [index, int(number)] for index, number in enumerate(boxes)
         ]
         result = []
-        for index, number in index_list:
-            operstions = 0
-            for inner_index, inner_number in index_list:
+        for index, number in enumerated_list:
+            operations = 0
+            for inner_index, inner_number in enumerated_list:
                 if inner_index == index:
                     continue
                 if inner_number == 1:
-                    operstions += abs(index - inner_index)
-            result.append(operstions)
+                    operations += abs(index - inner_index)
+            result.append(operations)
         return result
