14cf6594d79610d2952b9a83ad936f01cdf41c1f
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-07-02 00:38:04 +0300

    Time: 0 ms (100%), Space: 17.8 MB (52.68%) - LeetHub

--- 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.py
+++ 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.py
@@ -4,8 +4,8 @@ class Solution:
         
         for i in range(len(number)):
             if number[i] == digit:
-                current = number[:i] + number[i+1:]
-                if current > max_number:
-                    max_number = current
+                current_number = number[:i] + number[i+1:]
+                if current_number > max_number:
+                    max_number = current_number
         
         return max_number
