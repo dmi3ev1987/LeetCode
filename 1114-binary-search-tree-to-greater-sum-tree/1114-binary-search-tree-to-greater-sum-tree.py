266f273f54cf22b2e726bc6a909e9aeb02b99b79
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-07-19 11:13:51 +0300

    Time: 0 ms (100%), Space: 17.8 MB (60.51%) - LeetHub

---
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstToGst(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        tree_list = []
+        stack = []
+        current = root
+        
+        while current or stack:
+            while current:
+                stack.append(current)
+                current = current.left
+            current = stack.pop()
+            tree_list.append(current.val)
+            current = current.right
+        
+        current = root
+        sorted_tree_list = sorted(tree_list)
+
+        while current or stack:
+            while current:
+                stack.append(current)
+                current = current.left
+            current = stack.pop()
+            current.val = sum(sorted_tree_list[sorted_tree_list.index(current.val):])
+            current = current.right 
+        
+        return root
+        
