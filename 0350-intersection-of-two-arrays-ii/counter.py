6e0a540ef82c039ebac4ec369678172447ab80fd
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-07-26 14:30:40 +0000

    Counter. Intersection of Two Arrays II.

--- 0350-intersection-of-two-arrays-ii/counter.py
+++ 0350-intersection-of-two-arrays-ii/counter.py
@@ -7,7 +7,8 @@ def intersect(nums1, nums2):
     result = []
 
     # Итерируемся по элементам и их частотам в меньшем Counter
-    # чтобы потенциально ускорить процесс, если один массив значительно меньше другого
+    # чтобы потенциально ускорить процесс,
+    # если один массив значительно меньше другого
     if len(counts1) > len(counts2):
         counts1, counts2 = (
             counts2,
@@ -16,7 +17,8 @@ def intersect(nums1, nums2):
 
     for num, count1 in counts1.items():
         if num in counts2:
-            # Добавляем элемент минимальное количество раз, которое он встречается в обоих массивах
+            # Добавляем элемент минимальное количество раз,
+            # которое он встречается в обоих массивах
             count_in_intersection = min(count1, counts2[num])
             result.extend([num] * count_in_intersection)
 
