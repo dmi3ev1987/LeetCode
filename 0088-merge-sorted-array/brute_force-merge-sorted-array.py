32ba73397d5153a125a6385be07ff7bafdd021e7
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-07-28 21:47:01 +0000

    Brute force.

---
+++ 0088-merge-sorted-array/brute_force-merge-sorted-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def merge(
+        self, nums1: list[int], m: int, nums2: list[int], n: int
+    ) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for index in range(n):
+            nums1[m + index] = nums2[index]
+        nums1.sort()
