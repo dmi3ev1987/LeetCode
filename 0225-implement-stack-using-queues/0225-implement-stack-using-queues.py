69c2f22b58cd9a5534324162c86c4c5c1f2103db
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-07-28 14:46:58 +0000

    Ruff format.

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,24 +1,19 @@
 class MyStack:
-
     def __init__(self):
         self.stack = []
-        
 
     def push(self, value: int) -> None:
         self.stack.append(value)
-        
 
     def pop(self) -> int:
         if not self.empty():
             return self.stack.pop(-1)
         return None
-        
 
     def top(self) -> int:
         if not self.empty():
             return self.stack[-1]
         return None
-        
 
     def empty(self) -> bool:
         return not self.stack
