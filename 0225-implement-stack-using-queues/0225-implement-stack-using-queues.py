150097ad6afc075d35c390a8017a2921e1ff49e6
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-07-28 22:42:10 +0300

    Time: 0 ms (100%), Space: 17.9 MB (41.33%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,23 +1,23 @@
 class MyStack:
     def __init__(self):
-        self.stack = []
-        self.temp = []
+        self.queue = []
+        self.temp_queue = []
 
     def push(self, value: int) -> None:
-        self.temp.append(value)
-        while self.stack:
-            self.temp.append(self.stack.pop(0))
-        self.stack, self.temp = self.temp, self.stack
+        self.temp_queue.append(value)
+        while self.queue:
+            self.temp_queue.append(self.queue.pop(0))
+        self.queue, self.temp_queue = self.temp_queue, self.queue
 
     def pop(self) -> int:
         if not self.empty():
-            return self.stack.pop(0)
+            return self.queue.pop(0)
         return None
 
     def top(self) -> int:
         if not self.empty():
-            return self.stack[0]
+            return self.queue[0]
         return None
 
     def empty(self) -> bool:
-        return not self.stack
+        return len(self.queue) == 0
